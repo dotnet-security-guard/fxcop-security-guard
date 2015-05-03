f4f14a48a9adc206de309d8edaba86a2338e85d8
mcwww <babooshka57100@gmail.com>
2015-05-02 21:40:09 -0400

    Creation of WeakCipherAlgorithmRule (verify if DES is implemented)

---
+++ SecurityGuardTests/Tests/WeakCipherAlgorithmTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Tests
+{
+    class WeakCipherAlgorithmTest : FxCopTest
+    {
+
+        [TestCase]
+        public void testWeakCipherAlgorithm()
+        {
+            //System.Diagnostics.Debugger.Launch();
+
+            //Vulnerable case
+            getVerifier().assertIssueFound("WeakCipherAlgorithmRule", "WeakCipherAlgorithm.cs", 20);
+            getVerifier().assertIssueFound("WeakCipherAlgorithmRule", "WeakCipherAlgorithm.cs", 58);
+
+            //False positive verification
+            getVerifier().assertIssueCount(2, "WeakCipherAlgorithmRule", "WeakCipherAlgorithm.cs");
+        } 
+
+    }
+}
