3fbc680ccf853834af83e6bec46ec3af3c129ab1
mcwww <babooshka57100@gmail.com>
2015-05-25 23:49:47 -0400

    Creation of WeakCipherModeRule.cs (verify if ECB mode is used)

---
+++ SecurityGuardPlugin/Rules/WeakCipherModeRule.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.FxCop.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Rules
+{
+    class WeakCipherModeRule : BaseRule
+    {
+
+        public WeakCipherModeRule()
+            : base("SecurityGuard.WeakCipherMode")
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+
+        public override ProblemCollection Check(Member member)
+        {
+            Method meth = member as Method;
+            if (meth != null)
+            {
+                this.Visit(meth);
+            }
+
+            return Problems;
+        }
+
+        public override void VisitMethodCall(MethodCall call)
+        {
+            MemberBinding callee = (MemberBinding) call.Callee;
+
+            // namespace System.Security.Cryptography, CipherMode{CBC = 1,ECB = 2,OFB = 3,CFB = 4,CTS = 5}
+            if (callee.BoundMember.FullName.StartsWith("System.Security.Cryptography.SymmetricAlgorithm.set_Mode") && (System.Int32)((Literal)call.Operands[0]).Value == 2)  
+            {
+                this.Problems.Add(new Problem(this.GetResolution(), call));
+            }
+
+            base.VisitMethodCall(call);
+        }
+
+    }
+}
---
+++ SecurityGuardSamples/WeakCipherMode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuardSamples
+{
+    class WeakCipherMode
+    {
+
+        public static string EncryptECB(string decryptedString)
+        {
+            DESCryptoServiceProvider desProvider = new DESCryptoServiceProvider();
+            desProvider.Mode = CipherMode.ECB;
+            desProvider.Padding = PaddingMode.PKCS7;
+            desProvider.Key = Encoding.ASCII.GetBytes("e5d66cf8");
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(stream, desProvider.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] data = Encoding.Default.GetBytes(decryptedString);
+                    cs.Write(data, 0, data.Length);
+                    return Convert.ToBase64String(stream.ToArray());
+                }
+            }
+        }
+
+        public static string EncryptOFB(string decryptedString)
+        {
+            DESCryptoServiceProvider desProvider = new DESCryptoServiceProvider();
+            desProvider.Mode = CipherMode.OFB;
+            desProvider.Padding = PaddingMode.PKCS7;
+            desProvider.Key = Encoding.ASCII.GetBytes("e5d66cf8");
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(stream, desProvider.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] data = Encoding.Default.GetBytes(decryptedString);
+                    cs.Write(data, 0, data.Length);
+                    return Convert.ToBase64String(stream.ToArray());
+                }
+            }
+        }
+
+    }
+}
